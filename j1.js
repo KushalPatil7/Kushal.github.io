ea046ebaf5ee930c64e23b0b523f84521eb6de2d
KushalPatil7 <114641559+KushalPatil7@users.noreply.github.com>
2023-07-15 13:43:47 +0530

    Update j1.js

--- j1.js
+++ j1.js
@@ -3,13 +3,13 @@ var rn1= Math.random()*6;
 
 rn1=Math.floor(rn1)+1;
 var image1="dice"+rn1+".png";
-var src1="images/"+image1;
+var src1=image1;
 var im1=document.querySelectorAll("img")[0];
 im1.setAttribute("src",src1);
 var rn2= Math.random()*6;
 rn2= Math.floor(rn2)+1;
 var image2="dice"+rn2+".png";
-var src2="images/"+image2;
+var src2=image2;
 var im2= document.querySelectorAll("img")[1];
 im2.setAttribute("src",src2);
  if(rn1>rn2){
